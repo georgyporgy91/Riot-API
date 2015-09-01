60247e872cc5a46aa8984cba4e49b5e971ee4660
georgyporgy91 <georgyporgy91@gmail.com>
2015-08-31 23:31:04 -0400

    Create run.R

---
+++ run.R
@@ -0,0 +1,11 @@
+install.packages("shiny")
+install.packages("ggplot2")
+install.packages("scales")
+
+library(shiny)
+library(ggplot2)
+library(reshape2)
+library(scales)
+
+#setwd("")
+runApp("Riot-API")
